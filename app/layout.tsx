297f9a04aba122814e49584a654e4aa08cf3201c
M-Umar-Amir <ua74800@gmail.com>
2024-08-08 00:46:01 +0500

    1

--- app/layout.tsx
+++ app/layout.tsx
@@ -110,7 +110,6 @@ export default function RootLayout({
       >
         <Providers>
           <div className="relative flex flex-col h-screen">
-            <Navbar />
             <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
               {children}
             </main>
